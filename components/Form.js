c411ddfadd1a3c8d69b04a6421afe3d3e5e2b143
elyaperagov <piragof@gmail.com>
2023-01-02 18:25:49 +0300

    deploy

---
+++ components/Form.js
@@ -0,0 +1,76 @@
+
+// import "./styles.css";
+import React, { Component } from "react";
+import axios from "axios";
+
+let PATH = 'http://localhost:8000/api/index.php';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      sent: false,
+      error: null
+    }
+  }
+
+  FormSubmittionHandler(event) {
+    event.preventDefault();
+
+    axios({
+      method: 'post',
+      url: `${PATH}`,
+      headers: { 'content-type': 'application/json' },
+      data: this.state
+    })
+      .then(result => {
+        this.setState({
+          sent: result.data.sent
+        })
+      })
+      .catch(error => this.setState({ error: error.message }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Contact Me</h1>
+        <div className="container">
+          <form action="#">
+            <label>Full Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter your name.."
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
+            <br />
+            <label>Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+            <br />
+            <label>Subject</label>
+            <textarea
+              id="subject"
+              name="subject"
+              placeholder="Enter your message.."
+              value={this.state.message}
+              onChange={(e) => this.setState({ message: e.target.value })}
+            ></textarea>
+            <input type="submit" value="Submit" onClick={e => this.FormSubmittionHandler(e)} />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import Form from '../components/Form.js'
 import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,6 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Form />
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
